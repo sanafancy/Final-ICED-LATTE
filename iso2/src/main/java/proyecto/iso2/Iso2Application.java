d984edb21cc3ea89b3bc729a856538696bf685ed
sanafancy <xutingting197@gmail.com>
2025-03-06 20:05:07 +0100

    Primer commit del proyecto Spring Boot

---
+++ iso2/src/main/java/proyecto/iso2/Iso2Application.java
@@ -0,0 +1,13 @@
+package proyecto.iso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iso2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iso2Application.class, args);
+	}
+
+}
---
+++ iso2/src/test/java/proyecto/iso2/Iso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.iso2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iso2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
