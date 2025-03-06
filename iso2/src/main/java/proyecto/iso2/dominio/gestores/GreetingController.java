6c772b86daaa178c117689464b4e57289dc980b7
sanafancy <xutingting197@gmail.com>
2025-03-06 21:52:58 +0100

    Prueba con los archivos del greeting

---
+++ iso2/src/main/java/proyecto/iso2/dominio/gestores/GreetingController.java
@@ -0,0 +1,31 @@
+package proyecto.iso2.dominio.gestores;
+
+import proyecto.iso2.dominio.entidades.Greeting;
+import proyecto.iso2.resistencia.GreetingDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+@Controller
+public class GreetingController {
+    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+    @Autowired
+    private GreetingDAO greetingDAO;
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        log.info(greetingDAO.findAll().toString());
+        return "greeting";
+    }
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        Greeting savedGreeting = greetingDAO.save(greeting);
+        log.info("Saved greeting: " + savedGreeting);
+        return "result";
+    }
+}
