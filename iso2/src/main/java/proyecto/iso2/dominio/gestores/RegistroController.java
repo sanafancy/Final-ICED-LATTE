d61f177cc7495aa667a8312e23cb717810182892
sanafancy <xutingting197@gmail.com>
2025-03-11 18:25:21 +0100

    Posible cambio

--- iso2/src/main/java/proyecto/iso2/dominio/gestores/RegistroController.java
+++ iso2/src/main/java/proyecto/iso2/dominio/gestores/RegistroController.java
@@ -1,6 +1,7 @@
 package proyecto.iso2.dominio.gestores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import proyecto.iso2.dominio.entidades.Cliente;
 import proyecto.iso2.dominio.entidades.Restaurante;
@@ -9,7 +10,7 @@ import proyecto.iso2.resistencia.ClienteDAO;
 import proyecto.iso2.resistencia.RestauranteDAO;
 import proyecto.iso2.resistencia.RepartidorDAO;
 
-@RestController
+@Controller
 @RequestMapping("/registro")
 public class RegistroController {
 
@@ -22,18 +23,24 @@ public class RegistroController {
     @Autowired
     private RepartidorDAO repartidorDAO;
 
-    @PostMapping("/cliente")
-    public Cliente registrarCliente(@RequestBody Cliente cliente) {
-        return clienteDAO.save(cliente);
+    @PostMapping("/registroCliente")
+    public String registrarCliente(@RequestParam String nombre, @RequestParam String apellidos, @RequestParam String dni, @RequestParam String email, @RequestParam String pass) {
+        Cliente cliente = new Cliente(email, pass, nombre, apellidos, dni);
+        clienteDAO.save(cliente);
+        return "redirect:/login";
     }
 
-    @PostMapping("/restaurante")
-    public Restaurante registrarRestaurante(@RequestBody Restaurante restaurante) {
-        return restauranteDAO.save(restaurante);
+    @PostMapping("/registroRestaurante")
+    public String registrarRestaurante(@RequestParam String nombre, @RequestParam String cif, @RequestParam String email, @RequestParam String pass) {
+        Restaurante restaurante = new Restaurante(email, pass, nombre, cif);
+        restauranteDAO.save(restaurante);
+        return "redirect:/login";
     }
 
-    @PostMapping("/repartidor")
-    public Repartidor registrarRepartidor(@RequestBody Repartidor repartidor) {
-        return repartidorDAO.save(repartidor);
+    @PostMapping("/registroRepartidor")
+    public String registrarRepartidor(@RequestParam String nombre, @RequestParam String apellidos, @RequestParam String nif, @RequestParam String email, @RequestParam String pass) {
+        Repartidor repartidor = new Repartidor(email, pass, nombre, apellidos, nif, 0.0);
+        repartidorDAO.save(repartidor);
+        return "redirect:/login";
     }
 }
