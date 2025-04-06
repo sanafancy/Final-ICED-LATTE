bb7ed81b9251949c758c7e06c3d757e0d85d962b
Ingrid <ingrid.niveiro@alu.uclm.es>
2025-04-06 18:20:37 +0200

    Test RestauranteController básico realizado con éxito

--- iso2/src/test/java/proyecto/iso2/dominio/gestores/RestauranteControllerTest.java
+++ iso2/src/test/java/proyecto/iso2/dominio/gestores/RestauranteControllerTest.java
@@ -2,10 +2,21 @@ package proyecto.iso2.dominio.gestores;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
 public class RestauranteControllerTest {
 
-}
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
+    @Test
+    public void testContextLoads() {
+        // Verifica que el contexto de Spring se cargue correctamente y que el controlador esté disponible
+        assertNotNull(webApplicationContext.getBean(RestauranteController.class), "RestauranteController should be loaded in the application context");
+    }
+}
 
