a7f93ca607be8c5dab3d9a5c29324f1bce16fd0c
hamnana00 <chaesgf00@gmail.com>
2025-05-15 12:06:56 +0200

    Correcion de eliminacion de un solo item

--- iso2/src/main/java/proyecto/iso2/dominio/gestores/CartaMenuController.java
+++ iso2/src/main/java/proyecto/iso2/dominio/gestores/CartaMenuController.java
@@ -2,6 +2,7 @@ package proyecto.iso2.dominio.gestores;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
@@ -77,6 +78,7 @@ public class CartaMenuController {
     }
 
     @PostMapping("/editar/{cartaId}/eliminarItem/{itemId}")
+    @Transactional
     public String eliminarItem(@PathVariable Long cartaId, @PathVariable Long itemId) {
         itemMenuDAO.deleteById(itemId);
         return "redirect:/cartas/editar/" + cartaId;
--- iso2/src/main/java/proyecto/iso2/persistencia/ItemMenuDAO.java
+++ iso2/src/main/java/proyecto/iso2/persistencia/ItemMenuDAO.java
@@ -1,5 +1,8 @@
 package proyecto.iso2.persistencia;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import proyecto.iso2.dominio.entidades.*;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +15,8 @@ public interface ItemMenuDAO extends JpaRepository<ItemMenu, Long> {
     List<ItemMenu> findByCartaMenu(CartaMenu cartaMenu);
     @Transactional
     void deleteByCartaMenu(CartaMenu carta);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ItemMenu i WHERE i.id = :itemId")
+    void deleteById(@Param("itemId") Long itemId);
 }
